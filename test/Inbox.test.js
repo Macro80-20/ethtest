65cbe9016f6e86035a931afd84a439a689a3d8e5
Macro80/20 <fernando_lopezc@live.com>
2022-05-12 15:19:11 +0100

    Add test
    Deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,38 @@
 // contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('Web3')
+const {interface, bytecode} = require('../compile')
+// creates an instance of Web three and tells that instance to
+// attempt to connect to this local test network that we are hosting on our machine solely for the purpose
+// when we want to connect to other networks like rinkbey we would replace it with a different providers 
+const web3 = new Web3(ganache.provider())
+
+let accounts
+let inbox
+const initialMessage = "Hi there!"
+
+beforeEach( async() => {
+    accounts = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: [initialMessage]})
+    .send({from: accounts[0],gas: '1000000'})
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        // console.log(inbox)
+       assert.ok(inbox.options.address)
+    })
+    it("Has initial value" , async() => {
+        const message = await inbox.methods.message().call()
+        console.log(message)
+        assert.equal(message,initialMessage)
+    }) 
+    it("Will update message" , async() => {
+        const newMessage ="updatedMessage "
+        const message = await inbox.methods.setMessage(newMessage).send({from: accounts[0]})
+        const updatedMessage = await inbox.methods.message().call()
+        assert.equal(updatedMessage,newMessage)
+    }) 
+})
+
